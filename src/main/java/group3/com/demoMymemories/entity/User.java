b973c36cad95f1182d7594c2dc9c24bbd8d6d207
thannsoryalyga <thannsoryalyza@institute.pse.ngo>
2025-12-19 08:11:28 +0700

    Initial commit

---
+++ src/main/java/group3/com/demoMymemories/entity/User.java
@@ -0,0 +1,36 @@
+package group3.com.demoMymemories.entity;
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+
+}
+
